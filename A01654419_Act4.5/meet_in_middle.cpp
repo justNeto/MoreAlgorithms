32e3ef6729555c85e64f0b7d13d9c87a27e69485
justNeto <continental.lelgg.2b@gmail.com>
2022-11-17 13:55:28 -0600

    Added some stuff

---
+++ A01654419_Act4.5/meet_in_middle.cpp
@@ -0,0 +1,304 @@
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+/*
+   Programa hecho por github.com/justNeto
+   Matricula: A01654419
+   Alumno: Luis Ernesto Ladron de Guevara Gonzalez
+   Added some other stuff
+   Added some stuff
+*/
+
+bool debug;
+
+void prtVec(std::vector<double> vec)
+{
+	for (int i = 0; i < vec.size(); i++)
+	{
+		std::cout << "[" << vec[i] << "]";
+	}
+
+	std::cout << "\n";
+}
+
+void prtVecFrom(std::vector<double> vec, int left, int right)
+{
+	std::cout << "Searching from index " << left << " to index " << right << "\n";
+	for (int i = left; i < right; i++)
+	{
+		std::cout << "[" << vec[i] << "]";
+	}
+
+	std::cout << "\n";
+}
+
+/* int binary_search(double search, std::vector<double> arr, int left, int right) */
+/* { */
+/* 	// Array goes from arr[left... right] */
+/* 	// So if left <= right -> can still do the algoritm */
+/* 	if (debug) std::cout << "Left: " << left << "\n"; */
+/* 	if (debug) std::cout << "Right: " << right << "\n"; */
+
+/* 	if (left <= right) */
+/* 	{ */
+/* 		if (left + 1 == right) */
+/* 		{ */
+/* 			// Make last aseveration */
+/* 			if (arr[left] == search) */
+/* 			{ */
+/* 				return left; */
+/* 			} */
+/* 			else if (arr[right] == search) */
+/* 			{ */
+/* 				return right; */
+/* 			} */
+/* 			else */
+/* 			{ */
+/* 				return -1; */
+/* 			} */
+/* 		} */
+/* 		// Make a guess. Usually mid, but in this case randomized */
+
+/* 		unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count(); */
+/* 		std::srand(seed); // sets seed */
+
+/* 		int guess = std::rand()%(right-left + 1) + left; */
+
+/* 		if (debug) std::cout << "Guess is " << guess << "\n"; */
+/* 		//if (debug) std::cout << "Arr[guest] is " << arr[guess] << "\n"; */
+
+/* 		if (arr[guess] == search) */
+/* 		{ */
+/* 			return guess; */
+/* 		} */
+
+/* 		if (arr[guess] > search) // search in the lower part of the array */
+/* 		{ */
+/* 			if (debug) std::cout << "Searching in the left side of the array:\n"; */
+/* 			if (debug) prtVecFrom(arr, left, guess); */
+/* 			return binary_search(search, arr, left, guess); */
+/* 		} */
+/* 		else if (arr[guess] < search) */
+/* 		{ */
+/* 			if (debug) std::cout << "Searching in the right side of the array:\n"; */
+/* 			if (debug) prtVecFrom(arr, guess, right); */
+/* 			return binary_search(search, arr, guess, right); */
+/* 		} */
+/* 	} */
+
+/* 	return -1; */
+/* } */
+
+void meet_algorithm(std::vector<double> a, std::vector<double> b)
+{
+	std::cout << "Doing other stuff\n";
+}
+
+void meet_in_mid(std::vector<double> arr)
+{
+	if (debug) std::cout << "Start algorithm\n";
+
+	// First divide the array into two
+	std::vector<double> arr_a;
+	std::vector<double> arr_b;
+
+	int mid = arr.size()/2;
+
+	std::cout << mid;
+	std::cout << arr.size();
+
+	for (int i = 0; i < arr.size(); i++)
+	{
+		if (i >= mid)
+		{
+			// Place in arr_b
+			arr_b.push_back(arr[i]);
+		}
+		else
+		{
+			// Place in arr_a
+			arr_a.push_back(arr[i]);
+		}
+	}
+
+	prtVec(arr_a);
+	prtVec(arr_b);
+
+	/* std::vector<double> res_a = search();// Search in a */
+	/* std::vector<double> res_b = search();// Search in a */
+
+	/* // Meet in middle */
+	/* meet_algorithm(res_a, res_b); */
+
+	exit(0);
+
+	// First sort the list
+	/* sort(arr.begin(), arr.end()); */
+
+	/* std::cout << "Sorted list is \n"; */
+	/* prtVec(arr); */
+
+	/* if (search < arr[0]) */
+	/* { */
+	/* 	std::cout << "\nNumber is not present in the array\n"; */
+	/* 	exit(0); */
+	/* } */
+	/* else if (search > arr[arr.size()-1]) */
+	/* { */
+	/* 	std::cout << "\nNumber is not present in the array\n"; */
+	/* 	exit(0); */
+	/* } */
+
+	/* // This is the driver code for the algoritm */
+	/* int index = binary_search(search, arr, 0, arr.size()); */
+
+	/* if (index > -1) */
+	/* { */
+	/* 	std::cout << "\nNumber " << search << " is in the list at index " << index << "\n"; */
+	/* 	exit(0); */
+	/* } */
+	/* else */
+	/* { */
+	/* 	std::cout << "\nNumber is not present in the array\n"; */
+	/* 	exit(0); */
+	/* } */
+
+	/* // Once the array has been sorted start the random binary search */
+
+}
+
+void lineBreak()
+{
+	std::cout << "\n";
+}
+
+
+void setDebug()
+{
+	std::string ans;
+	std::cout << "\n::- [Activate debug mode]: 1 - yes | 0 - no (default)\n";
+
+	while (true)
+	{
+    		std::getline(std::cin, ans);
+
+		if (ans == "1")
+		{
+			std::cout << "| --- [ Activating debug mode ] --- | \n";
+			debug = true;
+
+			break;
+		}
+		else if (ans == "0")
+		{
+			std::cout << "| --- [ No debug mode ] --- | \n";
+			debug = false;
+
+			break;
+		}
+		else if (ans == "")
+		{
+			std::cout << "| --- [ No debug mode ] --- | \n";
+			debug = false;
+
+			break;
+		}
+		else
+		{
+			std::cout << "Please select an appropiate answer. Press enter to try again.";
+		}
+	}
+}
+
+void resBuff()
+{
+	std::cin.clear();
+	std::cin.ignore(10000, '\n');
+}
+
+int main()
+{
+	double sum_num;
+	std::vector<double> num_arr;
+
+	while (true)
+	{
+
+		std::cout << "Enter number to search: \n";
+
+		if (!(std::cin >> sum_num))
+		{
+			std::cout << "Error. Please enter a number.\n";
+			resBuff();
+			continue;
+		}
+
+		if (sum_num > std::pow(10,18))
+		{
+			std::cout << "The sum cannot be bigger than 10^18. Enter a smaller number\n";
+			exit(0);
+		}
+
+		std::cout << "Program will search for the number " << sum_num << "\n";
+
+		resBuff();
+
+		std::cout << "Enter List of numbers separated by commas: \n";
+
+		std::string nums_in;
+		std::getline(std::cin, nums_in); // gets the line of the numbers
+
+		// Parse the numbers
+		std::istringstream buf(nums_in);
+
+		while (!buf.eof())
+		{
+			double this_number;
+		      buf >> this_number;
+
+		      if (buf.bad())
+			{
+				std::cerr << "Number formatting error!\n";
+				std::cin.clear();
+				std::cin.ignore(10000, '\n');
+		      }
+
+		      num_arr.push_back(this_number);
+
+		      char comma = 0;
+		      buf >> comma;
+		      if (!buf.eof())
+			{
+				if (buf.fail())
+				{
+					exit(1);
+		            }
+		            if (comma!=',')
+				{
+					std::cerr << "Found no comma but '" << comma << "' instead !\n";
+					std::cerr << "Could not read comma.\n";
+					exit(1);
+		            }
+		      }
+		}
+		break;
+	}
+
+	setDebug();
+
+
+	if (num_arr.size() > 40)
+	{
+		std::cout << "The list cannot be bigger than 40\n";
+		exit(0);
+	}
+
+
+	meet_in_mid(num_arr);
+	return 0;
+}
